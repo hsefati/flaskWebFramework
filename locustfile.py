6b5360d220f15196974d1379ee69269f2a710b89
Hamed Sefati <hsefati@gmail.com>
2021-05-27 21:57:24 +0200

    change the locustfile

--- locustfile.py
+++ locustfile.py
@@ -2,26 +2,19 @@ import time
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2.5)
+    wait_time = between(1, 2)
 
     @task
-    def hello_world(self):
+    def home_page(self):
         self.client.get("/")
-        self.client.get("/")
-
-    @task(3)
-    def predict_items(self):
-        for item_id in range(10):
-            payload = { "CHAS":{"0":0},
-                        "RM":{"0":6.575},
-                        "TAX":{"0":296.0},
-                        "PTRATIO":{"0":15.3},
-                        "B":{"0":396.9},
-                        "LSTAT":{"0":4.98}
-                      }
-            
-            r = self.client.post("/predict", json=payload)
-            time.sleep(1)
-
-    def on_start(self):
-        print("This is locust without login")
+      
+    @task
+    def prediction(self):
+        self.client.post("/predict", 
+            json={"CHAS": { "0":0 },
+                  "RM":{ "0":6.575 },
+                  "TAX":{ "0":296.0 },
+                  "PTRATIO":{ "0":15.3 },
+                  "B":{ "0":396.9 },
+                  "LSTAT":{ "0":4.98 }         
+            })
